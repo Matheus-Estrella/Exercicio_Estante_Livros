01b0adf71f571f27cf54a776cb537575614995f2
Matheus-Estrella <matheus.a.estrella@gmail.com>
2024-04-29 20:43:48 -0300

    first commit

---
+++ Exercicio_Estante_Livros/Livro.cs
@@ -0,0 +1,81 @@
+﻿
+public class Livro
+{
+    public string Titulo;
+    public int N_Autores;
+    public string[] Autores = new string[3];
+    public DateOnly Data_Lancamento;
+    public string Editora;
+    public int Edicao;
+    public int ISBN;
+    public int Quantidade_Paginas;
+
+    public Livro() { }
+    public void Registrar_Novo_Livro()
+    {
+        Console.Write("\n Insira o título do livro: ");
+        this.Titulo = Console.ReadLine();
+
+        Console.WriteLine();
+
+        Console.Write("\n Insira a quantidade de autores: ");
+        N_Autores = int.Parse(Console.ReadLine())-1;
+
+        for (int i = 0; i <= N_Autores; i++)
+        {
+            Console.Write($" Digite o nome do {i + 1}º Autor: ");
+            this.Autores[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine();
+
+        Console.Write("\n Insira a data de lançamento do livro (DD/MM/AAAA): ");
+        this.Data_Lancamento = DateOnly.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.Write("\n Insira a Editora: ");
+        this.Editora = Console.ReadLine();
+
+        Console.WriteLine();
+
+        Console.Write("\n Insira o número da Edição: ");
+        this.Edicao = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.Write("\n Insira o código ISBN: ");
+        this.ISBN = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.Write("\n Insira a quantidade de páginas do livro: ");
+        this.Quantidade_Paginas = int.Parse(Console.ReadLine());
+
+    }
+    
+    public void Imprimir_Livro()
+    {
+        Console.WriteLine("\nTítulo: "+ this.Titulo);
+        
+        Console.WriteLine("\nAutor(es): ");
+        for (int i = 0; i < Autores.Length; i++)
+        {
+            if (this.Autores[i] != null && this.Autores[i] != "")
+            {
+                Console.WriteLine(this.Autores[i]);
+            }
+        }
+
+        Console.WriteLine("\n Data de Lançamento: " + this.Data_Lancamento);
+
+        Console.WriteLine("\n Editora: "+ this.Editora);
+
+        Console.WriteLine("\n Edição: "+ this.Edicao);
+
+        Console.WriteLine("\n Código ISBN: " + this.ISBN);
+
+        Console.WriteLine("\n Número de páginas: " + this.Quantidade_Paginas);
+    }
+}
+
---
+++ Exercicio_Estante_Livros/Program.cs
@@ -0,0 +1,133 @@
+﻿namespace Exercicio_Estante_Livros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 0, num=0;
+
+            Livro[] estante_livros = null;
+            do
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nBem vindo a estante de livros, você deseja:\n1-) Adicionar Livros\n2-) Consultar Livros");
+                    op = int.Parse(Console.ReadLine());
+                    if ((op < 1) || (op > 2))
+                    {
+                        Console.WriteLine("\nInsira uma opção válida");
+                    }
+                } while ((op < 1) || (op > 2));
+                Console.Clear();
+                switch (op)
+                {
+                    case 1:
+                        num = 0;
+                        if( estante_livros == null)
+                        {
+                            do
+                            {
+                                Console.Write("\nDigite quantos livros deseja adicionar na estante: ");
+                                num = int.Parse(Console.ReadLine());
+                                if (num <= 0)
+                                {
+                                    Console.WriteLine("\nInsira uma opção válida");
+                                }
+                            } while (num <= 0);
+                            estante_livros = new Livro[num];
+                        }
+                        else
+                        {
+                            do
+                            {
+                                Console.Write("\nDigite quantos livros deseja adicionar: ");
+                                num = int.Parse(Console.ReadLine());
+                                if (num <= 0)
+                                {
+                                    Console.WriteLine("\nInsira uma opção válida");
+                                }
+                            } while (num <= 0);
+                            Livro[] estante_atualizada = new Livro[estante_livros.Length + num];
+                            estante_livros.CopyTo(estante_atualizada,0);
+                            estante_livros = estante_atualizada;
+                        }
+                        for (int i = (estante_livros.Length - num ); i < estante_livros.Length; i++)
+                        {
+                            Console.Clear();
+                            estante_livros[i] = new Livro();
+                            estante_livros[i].Registrar_Novo_Livro();
+                        }
+                        break;
+                    case 2:
+                        Print_Estante(estante_livros, num);
+                        break;
+                }
+                op = 0;
+                Console.Clear();
+                Console.WriteLine("\nDeseja continuar a consultar a estante?\n1-) Sim\n2-) Não");
+                op = int.Parse(Console.ReadLine());
+            } while (op == 1);
+        }
+        static void Print_Estante(Livro[] estante, int tamanho_estante)
+        {
+            int inicio = 0, fim = 0, op = 0, n=0;
+            do
+            {
+                Console.Clear();
+                Console.Write("\nEscolha uma opção de consulta\n1-) Imprimir estante\n2-) Selecionar livro para imprimir\n" +
+                    "3-) Selecionar intervalo de livros para imprimir\n:");
+                op = int.Parse(Console.ReadLine());
+                if((op < 1) || (op > 3))
+                {
+                    Console.WriteLine("\nInsira uma opção válida");
+                }
+            } while ((op < 1) || (op > 3));
+            switch (op)
+            {
+                case 1:
+                    inicio = 0;
+                    fim = estante.Length;
+                    break;
+                case 3:
+                    do
+                    {
+                        Console.Clear();
+                        Console.Write("\nInsira o índice inicial para procura: ");
+                        inicio = int.Parse(Console.ReadLine())-1;
+                        Console.Write("\nInsira o índice final para procura: ");
+                        fim = int.Parse(Console.ReadLine());
+                        if(fim > estante.Length)
+                        {
+                            fim = estante.Length;
+                        }
+                        if ((inicio < 0) || (fim > estante.Length) || (inicio > fim))
+                        {
+                            Console.WriteLine("\nInsira uma opção válida");
+                        }
+                    } while ((inicio < 0) || (fim > estante.Length) || (inicio > fim));
+                    break;
+                case 2:
+                    do
+                    {
+                        Console.Clear();
+                        Console.Write("\nInsira o índice para procura: ");
+                        inicio = int.Parse(Console.ReadLine())-1;
+                        fim = inicio+1;
+                        if((inicio < 0) || (inicio > tamanho_estante))
+                        {
+                            Console.WriteLine("\nInsira uma opção válida");
+                        }
+                    } while ((inicio < 0) || (inicio > tamanho_estante));
+                    break;
+            }
+            for (int i = inicio; i < fim; i++)
+            {
+                Console.Clear();
+                estante[i].Imprimir_Livro();
+            }
+        }
+    }
+}
+
+
